a503b5ebde048490da5017d6064b51048c4d273d
xtz136 <xutaozhe@gmail.com>
2013-01-29 21:29:47 +0800

    remove old tests, improve test_schema

--- zapian/tests.py
+++ zapian/tests.py
@@ -138,6 +138,10 @@ class ZapianTest(unittest.TestCase):
         self.assertTrue(schema.get_prefix('new_field') == new_prefix == prefix)
         new_slot = schema.add_attribute('new_attribute')
         self.assertTrue(schema.get_slot('new_attribute') == new_slot == slot)
+        # test load the old schema
+        new_zapian = Zapian(self.data_root)
+        self.assertEqual(new_zapian.fields, {'new_field':'XA'})
+        self.assertEqual(new_zapian.attributes, {'new_attribute':0})
 
     def test_add_document(self, uid='123456', part=None):
         part = part if part is not None else self.parts[0]
@@ -177,112 +181,6 @@ class ZapianTest(unittest.TestCase):
 
     def test_search_document(self):
         pass
-
-    def atest_remove_database(self):
-        """ 测试删除数据库接口
-        """
-        engine.remove_part(self.site_name, self.catalog_name, self.parts[0])
-        database_path = os.path.join(self.data_root, self.site_name, self.catalog_name, self.parts[0])
-        self.assertTrue(not os.path.exists(database_path))
-        for part_name in self.parts[1:]:
-            database_path = os.path.join(self.data_root, self.site_name, self.catalog_name, part_name)
-            self.assertTrue(os.path.exists(database_path))
-
-    def atest_search_for_one_database(self, qs=None, search_uid='123456'):
-        """ 测试一个数据库搜索
-        """
-        if not qs:
-            qs = queryset.QuerySet()
-            qs.parse(u'we are', ['title'])
-            qs.filter(subjects__anyof=['file', 'ktv'])
-        result = engine.search(site_name = self.site_name,
-                                catalog_name = self.catalog_name,
-                                parts = self.parts[0],
-                                query_set = qs,
-                                )
-        self.assertTrue(search_uid in result)
-
-    def atest_search_for_multi_database(self):
-        """ 测试多个个数据库联合搜索
-        """
-        engine.add_document(site_name     = self.site_name,
-                            catalog_name  = self.catalog_name,
-                            part_name     = self.parts[1],
-                            uid           = 7890, 
-                            doc           = self.doc
-                            )
-        qs = queryset.QuerySet()
-        qs.parse(u'we are', ['title'])
-        qs.filter(subjects__anyof=['file', 'ktv'])
-        result = engine.search(site_name = self.site_name,
-                                catalog_name = self.catalog_name,
-                                parts = self.parts,
-                                query_set = qs,
-                                )
-        self.assertTrue('7890' in result)
-        self.assertTrue('123456' in result)
-
-    def atest_index_for_multi(self):
-        """ 测试一个数据库连续写入
-            同一路径写数据一次只能打开一个
-        """
-        # init the test data
-        self.doc = {'title':'missiong you', 
-                    'subjects':['while','white','watch'], 
-                    'created':datetime.now()}
-
-        engine.add_document(site_name     = self.site_name,
-                            catalog_name  = self.catalog_name,
-                            part_name     = self.parts[0],
-                            uid           = 123457, 
-                            doc           = self.doc
-                            )
-        # 默认搜索 123456 uid 的文档
-        self.assertEqual(self.test_search_for_one_database(), None)
-
-        # 搜索 123457 uid 的文档
-        qs = queryset.QuerySet()
-        qs.parse(u'you', ['title'])
-        qs.filter(subjects__allof=['while', 'white'])
-        self.assertEqual(self.test_search_for_one_database(qs, '123457'), None)
-
-        # 搜索错误的文档
-        qs = queryset.QuerySet()
-        qs.parse(u'we are', ['title'])
-        qs.filter(subjects__allof=['while', 'white'])
-        # 搜索不到, 会抛出assertion异常
-        self.assertRaises(AssertionError, self.test_search_for_one_database, qs)
-
-    def atest_replace_document(self):
-        """ 测试重建文档索引
-        """
-        new_doc = {'title':'new doc',
-                    'subjects':['new', 'doc'],
-                    'created':datetime.now()}
-
-        # 先正常搜索一次
-        self.assertEqual(self.test_search_for_one_database(), None)
-        # 重建文档索引
-        engine.replace_document(site_name = self.site_name,
-                                catalog_name = self.catalog_name,
-                                part_name = self.parts[0],
-                                uid = '123456',
-                                doc = new_doc,)
-        # 原来的应该搜索不到了, 就会抛出assertion异常
-        self.assertRaises(AssertionError, self.test_search_for_one_database)
-
-    def atest_update_document(self):
-        """ 测试更改文档某个字段的索引
-        """
-        # 先正常搜索一次
-        self.assertEqual(self.test_search_for_one_database(), None)
-        engine.update_document(site_name = self.site_name,
-                                catalog_name = self.catalog_name,
-                                part_name = self.parts[0],
-                                uid = '123456',
-                                doc = {'title':'new title'},)
-        # 原来的应该搜索不到了, 就会抛出assertion异常
-        self.assertRaises(AssertionError, self.test_search_for_one_database)
 
 if __name__ == '__main__':
 
