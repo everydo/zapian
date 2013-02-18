541579e3d7cb807376516d7100b55656471e24f3
xtz136 <xutaozhe@gmail.com>
2013-02-18 16:25:51 +0800

    delete unused methods

--- zapian/utils.py
+++ zapian/utils.py
@@ -21,47 +21,6 @@ def clean_value(value):
     else:
         raise TypeError('%s is not supported type.' % value)
 
-def process_doc(catalog_name, raw_doc):
-    """ 将原始的doc，根据catalog信息，转换为内部的doc形式，用于存放到队列
-
-       raw_doc: 原始的doc 
-
-           {field1:value1, 
-            field2:value2}
-
-       返回: 
-
-           {'fields':{"prefix:type":value}, 
-            'attributes':{"slot:type":12}, 
-            'data':{}}
-    """
-    internal_doc = {'sortable':{}, 'fields':{}, 'store':{}}
-    catalog = get_catalog(catalog_name)
-    for field, value in raw_doc.iteritems():
-
-        value = CLEANERS[type(value)](value, False)
-
-        # 一个field 可能同时有3种角色
-        if field in catalog.fields:
-            catalog_fields = catalog.fields[field]
-            prefix, types = catalog_fields['prefix'], catalog_fields['type']
-            internal_doc['fields'][field] = {"%s:%s"%(prefix, types): value}
-
-        if field in catalog.attributes:
-            catalog_fields = catalog.attributes[field]
-            slot, types = catalog_fields['slot'], catalog_fields['type']
-            internal_doc['sortable'][field] = {"%s:%s"%(slot, types): value}
-
-        if field in catalog.data:
-            try:
-                field_data = internal_doc['store'][field]
-            except KeyError:
-                field_data = []
-                internal_doc['store'][field] = field_data
-            field_data.append( value )
-
-    return internal_doc
-
 def datetimeNorm(value, is_query=False):
     value = int(mktime(value.timetuple()))
     return value
@@ -96,35 +55,4 @@ def clean_list(value):
         return " ".join(value)
     else:
         return CLEAN_LIST_RE.sub('_', value)
-
-def clean_parent(obj, is_query):
-    """ 得到一个folder的父容器ID """
-    if type(obj) == int:
-        # 用于搜索
-        return str(obj)
-    else:
-        return  str(pathNorm(obj, True)[0])
-
-def clean_path(objs, is_query):
-    """ 转换obj 为一个整数path
-
-    RETURN      由父对象ID组成的字符串 
-    """
-    value = []
-
-    if type(objs) in (tuple, list):
-        # 正在查询时调用
-        for obj in objs:
-            if type(obj) == int:
-                value.append(obj)
-            else:
-                value.extend(pathNorm(obj, True))
-    else:
-        # 建立索引时调用
-        value.extend(pathNorm(objs, False))
-
-    if not value:
-        return ""
-
-    return " ".join([ str(id) for id in value ])
 
