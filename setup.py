8a7b759a6171755415477b789e361e2770339c52
xtz136 <xutaozhe@gmail.com>
2013-04-03 12:32:32 +0800

    upgrade to 0.1.3dav

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 
 setup (
     name='zapian',
-    version='0.1.2dev',
+    version='0.1.3dev',
     author = "Pan Junyong",
     author_email = "dev@zopen.cn",
     description = "indexer addons: sort, cjksplitter, and so on",
